e31916a4902643ffdb168249c295923ce29762e3
Hrishi1807 <hrishikeshnangare18@gmail.com>
2023-11-10 00:05:56 +0530

    UserViewModel added with properties

---
+++ OperationTask/userCRUDUsingAPI/Model/UserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace userCRUDUsingAPI.Model
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
